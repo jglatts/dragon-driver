42baeb84b30f382dd606cfe3f7264bf88f7824fc
John Glatts <33476958+jglatts@users.noreply.github.com>
2019-05-12 12:22:14 -0400

    Update DragonDriver.cpp

--- DragonDriver/DragonDriver.cpp
+++ DragonDriver/DragonDriver.cpp
@@ -12,11 +12,11 @@
 
 
 /* Initialize the Dragon Driver */
-DragonDriver::DragonDriver(int step_pin, int dir_pin, int limit_pin) {
+DragonDriver::DragonDriver(int step_pin, int dir_pin, int limit_pin_front, int limit_pin_back) {
     _step_pin = step_pin;
     _dir_pin = dir_pin;
-    _limit_pin = limit_pin;
-
+    _limit_pin_front = limit_pin_front;
+    _limit_pin_back = limit_pin_back;
 }
 
 
@@ -24,14 +24,63 @@ DragonDriver::DragonDriver(int step_pin, int dir_pin, int limit_pin) {
 void DragonDriver::begin() {
     pinMode(_step_pin, OUTPUT);
     pinMode(_dir_pin, OUTPUT);
-    pinMode(_limit_pin, INPUT_PULLUP);
+    pinMode(_limit_pin_front, INPUT_PULLUP);
+    pinMode(_limit_pin_back, INPUT_PULLUP);
 }
+
+
+/* Test the speed */
+void DragonDriver::testSpeed() {
+    // re-set the pin after the for-loop
+    pinMode(_step_pin, OUTPUT);
+
+    // turn motor on and off
+    for (int i = 0; i < 3; ++i) {
+        analogWrite(_step_pin, 125);
+        delay(750);
+
+        // stop the motor shortly
+        pinMode(_step_pin, OUTPUT);
+        digitalWrite(_step_pin, LOW);
+        delay(1000);
+    }
+    pinMode(_step_pin, OUTPUT);
+    digitalWrite(_step_pin, LOW);
+    delay(1000);
+}
+
+
+/* Test the Dir, Verifying that front and back movement is working */
+void DragonDriver::testDirection() {
+    // keep stepping
+    for(int i=0;; i++){
+        if (!hasLimitFrontBeenReached()) {
+            // stop the motor
+            digitalWrite(_dir_pin, LOW);
+            analogWrite(_step_pin, 100);
+            delay(5000);
+        }
+        else if (!hasLimitBackBeenReached()) {
+            // stop the motor
+            digitalWrite(_dir_pin, HIGH);
+            analogWrite(_step_pin, 100);
+            delay(5400);
+        }
+        else {
+            // small steps to increase and then eventually tap the limit switch
+            digitalWrite(_dir_pin, HIGH);
+            analogWrite(_step_pin, 225);
+            delay(10);  // testing optimal delay time
+        }
+    }
+}
+
 
 
 /* Move the stepper clockwise, by the given steps */
 void DragonDriver::moveClockWise(int steps){
     // clean this up to a nice for-loop
-    if (hasLimitBeenReached()) {
+    if (hasLimitFrontBeenReached()) {
         // send motor back
         analogWrite(_step_pin, 0); // stop the motor
         delay(900);
@@ -53,7 +102,8 @@ void DragonDriver::moveClockWise(int steps){
 
 /* Move the stepper counter-clockwise, by the given steps */
 void DragonDriver::moveCounterClockWise(int steps){
-    if (hasLimitBeenReached()) {
+
+    if (hasLimitFrontBeenReached()) {
         // send motor back
         analogWrite(_step_pin, 0); // stop the motor
         delay(900);
@@ -72,9 +122,12 @@ void DragonDriver::moveCounterClockWise(int steps){
     delay(1000);
 }
 
-/* change this xD */
-bool DragonDriver::hasLimitBeenReached() {
-    // add more to this
-    return digitalRead(_limit_pin == HIGH); // debug this --> hopefully returns true or false
+
+bool DragonDriver::hasLimitFrontBeenReached() {
+    return digitalRead(_limit_pin_front == HIGH);
 }
 
+
+bool DragonDriver::hasLimitBackBeenReached() {
+    return digitalRead(_limit_pin_front == HIGH); 
+}
